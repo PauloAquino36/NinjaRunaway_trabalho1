eb5141daf74b608c64ef00074f424e5268a68bb1
Daniel Thielmann <daniel.thielmann@example.com>
2025-01-27 22:56:40 -0300

    starting to build a main menu

---
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+    public void Jogar()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    // Update is called once per frame
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        painelMenuInicial.SetActive(true);
+        painelOpcoes.SetActive(false);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
